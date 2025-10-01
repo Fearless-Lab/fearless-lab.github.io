66687741d7224cdc7bb39ef304e2732bc4bfd34b
KIMJINYOUNG <k5444862@naver.com>
2025-10-01 18:06:35 +0900

    Revert "feat: add prerender mode with Loading fallback for /banPickSimulation"
    This reverts commit 20b7ae27ac33077dda80613d85a7896b37059f3e.

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,6 @@ import BanPick from "@/pages/BanPick";
 import NotFound from "./pages/NotFound";
 import { BASE_URL } from "./../constants/url";
 import BanPickSimulation from "./pages/BanPickSimulation";
-import Loading from "./pages/components/BanPickSimulation/Loading";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -29,16 +28,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/banPick" element={<BanPick />} />
-            <Route
-              path="/banPickSimulation"
-              element={
-                import.meta.env.VITE_PRERENDER === "true" ? (
-                  <Loading />
-                ) : (
-                  <BanPickSimulation />
-                )
-              }
-            />
+            <Route path="/banPickSimulation" element={<BanPickSimulation />} />
 
             {/* <Route
               path="/community"
