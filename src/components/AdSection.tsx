345a612e2a7327cbcf5c5990c40a1ea6c72d6244
KIMJINYOUNG <k5444862@naver.com>
2026-01-26 22:14:01 +0900

    feat(ads): add Kakao AdFit responsive banners

--- src/components/AdSection.tsx
+++ src/components/AdSection.tsx
@@ -3,7 +3,14 @@ import KakaoAdFit from "./KakaoAdFit";
 const AdSection: React.FC = () => {
   return (
     <section className="w-full py-8 flex flex-col items-center gap-6">
-      <KakaoAdFit unit="DAN-Ji3L2DE01O29h5RC" width={320} height={100} />
+      {/* 모바일: 320x100 */}
+      <div className="block md:hidden">
+        <KakaoAdFit unit="DAN-Ji3L2DE01O29h5RC" width={320} height={100} />
+      </div>
+      {/* PC: 728x90 */}
+      <div className="hidden md:block">
+        <KakaoAdFit unit="DAN-YYwQ2W6dSwtLkMpt" width={728} height={90} />
+      </div>
     </section>
   );
 };
